608ce271bf08551fe2e8d9364aa97081358b8373
ViniRech <viniciusrech2007@gmail.com>
2023-07-17 10:54:22 -0300

    Main, footer e JS

---
+++ 2º Trabalho/js/script.js
@@ -0,0 +1,19 @@
+function diaDeHoje(diadehoje) {
+    const data = new Date();
+
+    let mes = (data.getMonth() === 0) ? ` de janeiro de `
+    : (data.getMonth() === 1) ? ` de fevereiro de `
+    : (data.getMonth() === 2) ? ` de março de `
+    : (data.getMonth() === 3) ? ` de abril de `
+    : (data.getMonth() === 4) ? ` de maio de `
+    : (data.getMonth() === 5) ? ` de junho de `
+    : (data.getMonth() === 6) ? ` de julho de `
+    : (data.getMonth() === 7) ? ` de agosto de `
+    : (data.getMonth() === 8) ? ` de setembro de `
+    : (data.getMonth() === 9) ? ` de outubro de `
+    : (data.getMonth() === 10) ? ` de novembro de `
+    : ` de dezembro de `;
+    
+    let dataAtual = data.getDate() + mes + data.getFullYear();
+    document.write(dataAtual);
+}
